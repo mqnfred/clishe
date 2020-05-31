722ea24f27a1ce179bcc6bda05ed01ecd0e4207e
Louis Feuvrier <mqnfred@gmail.com>
2020-05-30 19:13:41 -0700

    Hide shell behind a feature flag
    The shell feature depends on rustyline+shellwords, the first of which is
    very involved. We can save precious compilation time by removing this
    for all users who do not need it.

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ pub mod prelude {
     pub use ::anyhow::{Error,Result};
     pub use ::clap::Clap as _;
     pub use crate::Command;
+    #[cfg(feature = "shell")]
     pub use crate::Shell;
 }
 
@@ -16,9 +17,11 @@ pub trait Command<C, R> {
     fn run(self, ctx: &mut C) -> ::anyhow::Result<R>;
 }
 
+#[cfg(feature = "shell")]
 pub struct Shell<C, R, A: ::clap::Clap + Command<C, R>> {
     _phda: ::std::marker::PhantomData<A>,
     _phdc: ::std::marker::PhantomData<C>,
     _phdr: ::std::marker::PhantomData<R>,
 }
+#[cfg(feature = "shell")]
 mod shell;
